3b1823c665077f6253ac0ee2d1536d6bed611be3
pavan143888 <pavanbalineni@gmail.com>
2020-12-21 13:17:43 +0530

    teting java handler

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -27,7 +27,7 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
                 .withHeaders(headers);
         try {
             final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"hello world-10\", \"location\": \"%s\" }", pageContents);
+            String output = String.format("{ \"message\": \"hello world-11\", \"location\": \"%s\" }", pageContents);
 
             return response
                     .withStatusCode(200)
